96085e4c93cbe968367eaf82bd49dcbd8d32cd36
kamalakshancg <kamalaksha.ncg@gmail.com>
2021-10-06 09:20:19 +0530

    stage_01 is completed

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -1,4 +1,4 @@
-from src.utils.all_utils import read_yaml
+from src.utils.all_utils import read_yaml,create_dir
 import argparse
 import pandas as pd
 import os
@@ -9,8 +9,30 @@ def get_data(config_path):
     data_path = config["data_source"]
 
     train = pd.read_csv(data_path, sep=";")
-
     print(train.head())
+    
+    artifacts_dir = config['artifacts']['artifacts_dir']
+    raw_local_dir = config['artifacts']['raw_local_dir']
+    raw_local_file = config['artifacts']['raw_local_file']
+
+
+    raw_local_dir_path = os.path.join(artifacts_dir,raw_local_dir)
+
+    create_dir(dirs=[raw_local_dir_path])
+
+    raw_local_path_file = os.path.join(raw_local_dir_path,raw_local_file)
+    
+    train.to_csv(raw_local_path_file,sep=";",index=False)
+    
+
+
+
+
+#     artifacts:
+#   artifacts_dir: artifacts
+#   raw_local_dir: raw_local_dir
+#   raw_local_file: data.csv
+
 
 
 if __name__ == "__main__":
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -6,3 +6,9 @@ def read_yaml(path_to_yaml):
         content = yaml.safe_load(yaml_file)
 
     return content
+
+def create_dir(dirs):
+    for each_dir in dirs:
+        os.makedirs(each_dir,exist_ok=True)
+        print("directory is created at",each_dir)
+    
